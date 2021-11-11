4290b9d8521c386bd0019b947bbcdfa36963f6f6
jbmakib <mdfoujolmobin@gmail.com>
2021-11-11 09:17:54 +0600

    banner image added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,19 @@
 import React from "react";
+import bannerImage from "../../../images/banner-image.png";
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner</h2>
+        <div className="container">
+            <div className="row gx-5 align-items-center">
+                <div className="col"></div>
+                <div className="col">
+                    <img
+                        src={bannerImage}
+                        alt="Biker is riding"
+                        className="w-100"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
