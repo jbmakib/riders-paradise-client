13bb51ced034b9cdc845d0d3e383b980ee392d7e
jbmakib <mdfoujolmobin@gmail.com>
2021-11-11 16:43:01 +0600

    dashboard added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import AuthProvider from "./contexts/AuthProvider";
+import Dashboard from "./Pages/Dashboard/Dashboard/Dashboard";
 import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login/Login";
+import PrivateRoute from "./Pages/Login/PrivateRoute/PrivateRoute";
 import Register from "./Pages/Login/Register/Register";
 import NotFound from "./Pages/NotFound/NotFound";
 
@@ -20,6 +22,9 @@ function App() {
                     <Route path="/register">
                         <Register />
                     </Route>
+                    <PrivateRoute path="/dashboard">
+                        <Dashboard />
+                    </PrivateRoute>
                     <Route path="*">
                         <NotFound />
                     </Route>
