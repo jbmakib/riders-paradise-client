79b1b662ecfab52413b307944533f3cd8fa20e7e
jbmakib <mdfoujolmobin@gmail.com>
2021-11-11 09:45:57 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,12 +1,22 @@
 import React from "react";
 import bannerImage from "../../../images/banner-image.png";
+import "./Banner.css";
 
 const Banner = () => {
     return (
-        <div className="container">
-            <div className="row gx-5 align-items-center">
-                <div className="col"></div>
-                <div className="col">
+        <div className="container banner">
+            <div className="row gx-3 align-items-center">
+                <div className="col-12 col-lg-6 bigger-text fw-bold">
+                    <h6 className="text-uppercase">Money can't buy</h6>
+                    <h1 className="text-primary text-uppercase">Happiness</h1>
+                    <h3 className="text-uppercase">
+                        but<span className="text-primary">...</span>
+                    </h3>
+                    <h1 className="banner-focus">
+                        Bike <span className="text-primary">Can</span>
+                    </h1>
+                </div>
+                <div className="col-12 col-lg-6">
                     <img
                         src={bannerImage}
                         alt="Biker is riding"
