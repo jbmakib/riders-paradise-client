0b609c03d1d29f6f08e13406b69ea0e728a73691
jbmakib <mdfoujolmobin@gmail.com>
2021-11-11 08:55:15 +0600

    footer created and home banner added to home

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "../../Shared/Footer/Footer";
 import Header from "../../Shared/Header/Header";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Banner />
+            <Footer />
         </div>
     );
 };
