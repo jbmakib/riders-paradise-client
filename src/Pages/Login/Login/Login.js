57c132f952495230f02e6e95299bd921610b74b0
jbmakib <mdfoujolmobin@gmail.com>
2021-11-11 10:37:02 +0600

    login image added and login updated

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import loginImage from "../../../images/login-image.png";
 
 const Login = () => {
+    const [loginInfo, setLoginInfo] = useState({});
+
+    const handleOnInput = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginInfo = { ...loginInfo };
+        newLoginInfo[field] = value;
+        setLoginInfo(newLoginInfo);
+    };
+
+    const handleLoginForm = (e) => {
+        e.preventDefault();
+        console.log(loginInfo);
+    };
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className="container">
+            <div className="row gx-3 align-items-center">
+                <div className="col-12 col-lg-6 text-center">
+                    <h1 className="text-primary">Login</h1>
+                    <form className="w-75 mx-auto" onSubmit={handleLoginForm}>
+                        <div className="form-floating mb-3">
+                            <input
+                                type="email"
+                                required
+                                className="form-control"
+                                id="floatingEmail"
+                                placeholder="Your Email"
+                                name="email"
+                                onInput={handleOnInput}
+                            />
+                            <label htmlFor="floatingEmail">Email address</label>
+                        </div>
+                        <div className="form-floating">
+                            <input
+                                type="password"
+                                required
+                                className="form-control"
+                                id="floatingPassword"
+                                placeholder="Password"
+                                name="password"
+                                onInput={handleOnInput}
+                            />
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary my-3">
+                            Login
+                        </button>
+                    </form>
+                </div>
+                <div className="col-12 col-lg-6">
+                    <img src={loginImage} alt="Login" className="w-100" />
+                </div>
+            </div>
         </div>
     );
 };
