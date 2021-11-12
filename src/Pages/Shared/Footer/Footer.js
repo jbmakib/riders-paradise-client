4413479dfcb278449ebb2a8de063532fca1f14bd
jbmakib <mdfoujolmobin@gmail.com>
2021-11-12 10:28:16 +0600

    footer updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -34,7 +34,7 @@ const Footer = () => {
                         <p className="fw-bold">Discover Our App</p>
                         <div className="d-flex justify-content-between">
                             <a
-                                className="btn btn-outline-orange"
+                                className="btn btn-outline-primary"
                                 href="https://play.google.com/"
                                 target="_blank"
                                 rel="noreferrer"
@@ -42,7 +42,7 @@ const Footer = () => {
                                 Google Play
                             </a>
                             <a
-                                className="btn btn-outline-orange"
+                                className="btn btn-outline-primary"
                                 href="https://www.apple.com/app-store/"
                                 target="_blank"
                                 rel="noreferrer"
