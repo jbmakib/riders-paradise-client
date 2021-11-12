81f46e26e2b94eead19ed45bacb6a21848cd6b70
jbmakib <mdfoujolmobin@gmail.com>
2021-11-12 21:43:22 +0600

    admin route updated

--- src/Pages/Login/AdminRoute/AdminRoute.js
+++ src/Pages/Login/AdminRoute/AdminRoute.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Link } from "react-router-dom";
 import useAuth from "./../../../hooks/useAuth";
 
 const AdminRoute = ({ children, ...rest }) => {
     const { user, isAdmin, isLoading } = useAuth();
-    if (isLoading || !isAdmin) {
+    if (isLoading) {
         return (
             <div className="text-center">
                 <div className="spinner-border text-purple" role="status"></div>
+            </div>
+        );
+    }
+    if (!isAdmin) {
+        return (
+            <div className="text-center">
+                <h2>You don't have any access to visit admin panel</h2>
+                <Link to="/dashboard">&#8592; Visit your dashboard</Link>
             </div>
         );
     }
