9430d05ec0598ce1d356c355b35e4cbf2456ec61
jbmakib <mdfoujolmobin@gmail.com>
2021-11-12 10:28:04 +0600

    products section updated

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Product from "../../Shared/Product/Product";
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("/products.json")
+            .then((res) => res.json())
+            .then((data) => setProducts(data.slice(0, 6)));
+    }, []);
     return (
         <div className="container">
-            <h2>This is products</h2>
+            <h2>This is products {products.length}</h2>
+            {products.length === 0 ? (
+                <div className="text-center">
+                    <div
+                        className="spinner-border text-primary"
+                        role="status"
+                    ></div>
+                </div>
+            ) : (
+                <div className="g-4 row row-cols-lg-3 row-cols-md-2 row-cols-1">
+                    {products.map((product, index) => (
+                        <Product key={index} product={product} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
---
+++ src/Pages/Shared/Product/Product.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import "./Product.css";
+
+const Product = ({
+    product: { _id, name, img, price, ownerShipSerial, kilometers, ratings },
+}) => {
+    const reactStartsInfo = {
+        value: ratings.rate,
+        edit: false,
+        isHalf: true,
+        emptyIcon: <i className="far fa-star" />,
+        halfIcon: <i className="fa fa-star-half-alt" />,
+        filledIcon: <i className="fa fa-star" />,
+    };
+
+    return (
+        <div className="col">
+            <div className="h-100 rounded-4 overflow-hidden card">
+                <img
+                    className="card-img-top border-bottom border-4 border-primary"
+                    src={img}
+                    alt={name}
+                />
+                <div className="card-body">
+                    <div className="fw-bold border-bottom pb-3 border-3 card-border d-flex justify-content-between">
+                        <div className="text-muted d-flex">
+                            <ReactStars {...reactStartsInfo} />
+                            <span className="ps-1">
+                                {ratings?.rate} ({ratings?.raters})
+                            </span>
+                        </div>
+                        <div>
+                            <i className="fas fa-tachometer-alt text-primary"></i>
+                            <span className="text-muted ps-1">
+                                {kilometers} km
+                            </span>
+                        </div>
+                    </div>
+                    <div className="pt-3 fw-bold card-title h5">{name}</div>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <small className="m-0 text-secondary">
+                            {ownerShipSerial} Ownership
+                        </small>
+                        <p className="h5 m-0 text-primary">${price}</p>
+                    </div>
+                    {/* <Link to={`/book-service/${_id}`}>
+                        <button className="rounded-pill px-3 py-1 mt-1 btn-outline-primary">
+                            Book
+                        </button>
+                    </Link> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
