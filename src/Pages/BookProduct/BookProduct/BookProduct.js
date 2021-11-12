879efcbdf3475da4ccac061c07cdd56dde8768a8
jbmakib <mdfoujolmobin@gmail.com>
2021-11-12 18:26:44 +0600

    book product page added

--- src/Pages/BookProduct/BookProduct/BookProduct.js
+++ src/Pages/BookProduct/BookProduct/BookProduct.js
@@ -1,35 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import Footer from "../../Shared/Footer/Footer";
 import Header from "../../Shared/Header/Header";
 
 const BookProduct = () => {
+    const [error, setError] = useState("");
     const [product, setProduct] = useState({});
     const { _id } = useParams();
 
     useEffect(() => {
-        fetch(`http://localhost:5000/products/${_id}`)
+        fetch(`https://enigmatic-refuge-60972.herokuapp.com/products/${_id}`)
             .then((res) => res.json())
             .then((data) => setProduct(data))
-            .catch((err) => {});
+            .catch((err) => setError("Invalid request to server"));
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    if (!product._id) {
+        return (
+            <div>
+                <Header />
+                {error ? (
+                    <>
+                        <div className="h4 text-center my-5 fw-bold text-purple">
+                            {error}
+                        </div>
+                        <Link to="/">
+                            <div className="text-center">
+                                &#8592; Back to home
+                            </div>
+                        </Link>
+                    </>
+                ) : (
+                    <div className="text-center my-5">
+                        <div
+                            className="spinner-border text-purple"
+                            role="status"
+                        ></div>
+                    </div>
+                )}
+                <Footer />
+            </div>
+        );
+    }
 
     return (
         <div>
             <Header />
-            {product._id ? (
-                <div className="container">
-                    <h2>Book product: {product.name}</h2>
-                </div>
-            ) : (
-                <div className="text-center">
-                    <div
-                        className="spinner-border text-purple"
-                        role="status"
-                    ></div>
-                </div>
-            )}
+            <div className="container">
+                <h2>Book product: {product.name}</h2>
+            </div>
             <Footer />
         </div>
     );
