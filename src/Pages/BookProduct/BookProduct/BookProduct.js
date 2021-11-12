cbe2b218358a404ee96f66e5f7516b59376898c0
jbmakib <mdfoujolmobin@gmail.com>
2021-11-12 17:14:10 +0600

    book product updated

--- src/Pages/BookProduct/BookProduct/BookProduct.js
+++ src/Pages/BookProduct/BookProduct/BookProduct.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Footer from "../../Shared/Footer/Footer";
 import Header from "../../Shared/Header/Header";
 
 const BookProduct = () => {
+    const [product, setProduct] = useState({});
     const { _id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/products/${_id}`)
+            .then((res) => res.json())
+            .then((data) => setProduct(data))
+            .catch((err) => {});
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <div>
             <Header />
-            <div className="container">
-                <h2>Book product: {_id}</h2>
-            </div>
+            {product._id ? (
+                <div className="container">
+                    <h2>Book product: {product.name}</h2>
+                </div>
+            ) : (
+                <div className="text-center">
+                    <div
+                        className="spinner-border text-purple"
+                        role="status"
+                    ></div>
+                </div>
+            )}
             <Footer />
         </div>
     );
