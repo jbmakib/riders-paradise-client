50a2e4dac019a9569a7bde17b56647e61910147e
jbmakib <mdfoujolmobin@gmail.com>
2021-11-13 18:09:09 +0600

    reviews section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Footer from "../../Shared/Footer/Footer";
 import Header from "../../Shared/Header/Header";
 import Banner from "../Banner/Banner";
 import Products from "../Products/Products";
+import Reviews from "../Reviews/Reviews";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Header />
             <Banner />
             <Products />
+            <Reviews />
             <Footer />
         </div>
     );
---
+++ src/Pages/Home/Reviews/Reviews.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch("/reviews.json")
+            .then((res) => res.json())
+            .then((data) => setReviews(data))
+            .catch((err) => console.log(err.message));
+    }, []);
+
+    return (
+        <div className="container my-3">
+            <h2 className="text-center">Reviews {reviews.length}</h2>
+        </div>
+    );
+};
+
+export default Reviews;
