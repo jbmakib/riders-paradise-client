9c87c68f98d2580c5c2e700ea766084fff758fc7
jbmakib <mdfoujolmobin@gmail.com>
2021-11-13 18:41:06 +0600

    reviews UI updated

---
+++ src/Pages/Home/Review/Review.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+
+const Review = ({
+    review: { name, title, description, designation, company, email, ratings },
+}) => {
+    const reactStartsInfo = {
+        value: ratings,
+        edit: false,
+        isHalf: true,
+        emptyIcon: <i className="far fa-star" />,
+        halfIcon: <i className="fa fa-star-half-alt" />,
+        filledIcon: <i className="fa fa-star" />,
+    };
+
+    return (
+        <div className="col">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <div className="d-flex my-2">
+                        <ReactStars {...reactStartsInfo} />
+                        <span className="ps-1">({ratings})</span>
+                    </div>
+                    <p className="card-text">{description}</p>
+                </div>
+                <div className="card-footer py-3">
+                    <h6 className="card-subtitle mb-2">{name}</h6>
+                    <small className="d-block">
+                        {designation} at {company}
+                    </small>
+                    <small className="d-block">{email}</small>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Review;
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Review from "../Review/Review";
 
 const Reviews = () => {
     const [reviews, setReviews] = useState([]);
@@ -11,7 +12,21 @@ const Reviews = () => {
 
     return (
         <div className="container my-3" id="reviews">
-            <h2 className="text-center">Reviews {reviews.length}</h2>
+            <h2 className="text-center">Reviews</h2>
+            {reviews.length === 0 ? (
+                <div className="text-center">
+                    <div
+                        className="spinner-border text-purple"
+                        role="status"
+                    ></div>
+                </div>
+            ) : (
+                <div className="g-4 row row-cols-lg-4 row-cols-md-2 row-cols-1">
+                    {reviews.map((review, index) => (
+                        <Review key={index} review={review} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
